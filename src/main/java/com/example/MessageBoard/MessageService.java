df819cecd408d28e5fda25290789ba33b7abc00c
ikennot <domdomkenneth23@gmail.com>
2025-07-24 18:25:46 +0800

    create codes for Message service

--- src/main/java/com/example/MessageBoard/MessageService.java
+++ src/main/java/com/example/MessageBoard/MessageService.java
@@ -1,3 +1,45 @@
+
+package main.java.com.example.MessageBoard;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+@Service
 public class MessageService {
+	
+
+    private List<Message> messages  = new ArrayList<>();
+
+    public List<Message> getMessage(){
+        return messages;
+    }
+
+    public void addMessage(Message message){
+        messages.add(message);
+    }
     
+    public Message getMessageId(long msgId){
+         for (Message message : messages) {
+              if(message.getMsgId() == msgId)
+                 return message;
+         }
+
+       return null;
+    }
+
+    public void updateMessage(long msgId, Message updMessage){
+        for (Message message: messages){
+            if(message.getMsgId() == msgId)
+            {
+            message.setMessage(updMessage.getMessage());
+            }
+        }
+    }
+   
+    public void deleteMessage(long msgId){
+        messages.remove(msgId);
+    }
+
+
 }
