f95da30c84c939d4db8e3d54e0c3554f66eed377
ikennot <domdomkenneth23@gmail.com>
2025-07-25 19:35:25 +0800

    adding html files

--- src/main/java/com/example/MessageBoard/MvcController.java
+++ src/main/java/com/example/MessageBoard/MvcController.java
@@ -1,20 +1,37 @@
 package com.example.MessageBoard;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/messageboardweb")
 public class MvcController {
     
-    @GetMapping("/index")
-public String showIndex(){
-  return "index";
-}
+  @Autowired
+   private MessageService service;
 
-@GetMapping("/AddMessage")
-public String messageForm(){
+  @GetMapping("/index")
+   public String showIndex(Model model){
+    model.addAttribute("message",service.getMessages());
+    return "index";
+  }
+
+  @GetMapping("/AddMessage")
+  public String messageForm(Model model){
+    model.addAttribute("message",new Message());
     return "form";
-}
+  }
+
+  
+  @PostMapping("/addnewmessage")
+   public String addnewMessage(@ModelAttribute Message message,Model model){
+    service.addMessage(message);
+  return "redirect:/messageboardweb/index"; // redirect para maiwasan ang double-submit
+   }
+
 }
